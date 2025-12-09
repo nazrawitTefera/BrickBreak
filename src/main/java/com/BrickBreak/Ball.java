6f6fc32e1967c0dbcfa6e2facbb3b444a6c91a60
naz <nazikefyalew@gmail.com>
2025-12-09 18:36:10 +0000

    first

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -5,11 +5,15 @@ public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int ypos;
+	private int xpos;
+	private int size;
+	private int xVelocity;
+	private int yVelocity;
+
 	
 	//constructor(s):
-	
+	public ball(int x, int y, int size)
 	//methods:
 
 }
